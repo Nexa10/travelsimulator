149b69a293d5758f1d50907f4e3a9cf4abfc2cd7
Dennis Audu <dennisaudu@Nexa-Macbook-Air.local>
2023-10-03 17:34:44 -0400

    made changes

--- TravelSimulation.java
+++ TravelSimulation.java
@@ -1,32 +1,36 @@
 import java.text.DecimalFormat;
 
-class TravelSimulation {
-    public static void printVehicleInformation(Vehicle vehicle, String vehicleType) {
+class VehicleUtility {
+    public void printVehicleInformation(Vehicle vehicle, String vehicleType) {
         DecimalFormat df = new DecimalFormat("#.###");
         DecimalFormat df2 = new DecimalFormat("#.#");
         System.out.println("Vehicle Type: " + vehicleType);
         System.out.println("Travel Distance: " + df.format(vehicle.getDistance())+ "km");
         System.out.println("Travel Time: " + df.format(vehicle.getTime()) + " hours");
         System.out.println("Current Fuel Level: " + df2.format(vehicle.getFuelLevel()) + "L");
-        System.out.println("Fuel Cost: $" + vehicle.getFuelCost());
+        System.out.println("Fuel Cost: $" +  df2.format(vehicle.getFuelCost()));
         System.out.println();
     }
+}
+
+class TravelSimulation {
     public static void main(String[] args) {
+        VehicleUtility vehicleUtility = new VehicleUtility();
         
         // the boat travels 100km and information about the boat is printed
         Vehicle boat = new BoatAdapter();
         boat.travel(100.0);
-        printVehicleInformation(boat, "Boat");
+        vehicleUtility.printVehicleInformation(boat, "Boat");
 
         // the car travels 1000km and information about the car is printed
         Vehicle car = new CarAdapter();
         car.travel(1000.0);
-        printVehicleInformation(car, "Car");
+        vehicleUtility.printVehicleInformation(car, "Car");
 
         // the airplane travels 10000km and information about the airplane is printed
         Vehicle airplane = new AirplaneAdapter();
         airplane.travel(10000.0);
-        printVehicleInformation(airplane, "Airplane");
+        vehicleUtility.printVehicleInformation(airplane, "Airplane");
 
         // Add fuel to each
         System.out.println();
@@ -40,15 +44,15 @@ class TravelSimulation {
         // Travel again
         // Boat
         boat.travel(100.0);
-        printVehicleInformation(boat, "Boat");
+        vehicleUtility.printVehicleInformation(boat, "Boat");
 
         // Car
         car.travel(1000.0);
-        printVehicleInformation(car, "Car");
+        vehicleUtility.printVehicleInformation(car, "Car");
 
         // Airplane
         airplane.travel(10000.0);
-        printVehicleInformation(airplane, "Airplane");
+        vehicleUtility.printVehicleInformation(airplane, "Airplane");
 
         new java.util.Scanner(System.in).nextLine();
     }
